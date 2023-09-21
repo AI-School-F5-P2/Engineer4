cb6e5363e41b10797c89b33ade274b82ae34812c
AlexVennegas <erick.alexis.venegas@gmail.com>
2023-09-21 08:48:48 +0200

    Feat: created faust-app, and functions load

---
+++ Functions/Push_name.py
@@ -0,0 +1,36 @@
+from MongoDB.Conexion import *
+
+# Push data to MongoDB
+def push_name(data, name):
+    item_to_search = data[name]
+    result = db[MONGO_COLLECTION].find_one({"name" : item_to_search})
+    if result is None:
+        db[MONGO_COLLECTION].insert_one(data)            
+    else:
+        db[MONGO_COLLECTION].update_one({"name": item_to_search}, {"$set": data})
+
+# Push data to MongoDB
+def push_address(data, name):
+    item_to_search = data[name]
+    result = db[MONGO_COLLECTION].find_one({"Address" : item_to_search})
+    if result is None:
+        db[MONGO_COLLECTION].insert_one(data)            
+    else:
+        db[MONGO_COLLECTION].update_one({"Address": item_to_search}, {"$set": data})
+    
+def push_fullname(data, name):
+    item_to_search = data[name]
+    result = db[MONGO_COLLECTION].find_one({"Fullname" : item_to_search})
+    if result is None:
+        db[MONGO_COLLECTION].insert_one(data)            
+    else:
+        db[MONGO_COLLECTION].update_one({"Fullname": item_to_search}, {"$set": data})
+
+
+def push_passport(data, name):
+    item_to_search = data[name]
+    result = db[MONGO_COLLECTION].find_one({"Passport" : item_to_search})
+    if result is None:
+        db[MONGO_COLLECTION].insert_one(data)            
+    else:
+        db[MONGO_COLLECTION].update_one({"Passport": item_to_search}, {"$set": data})
---
+++ MongoDB/Conexion.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+
+# CONFIGURACION CREENCIALES DE MONGOBD
+MONGO_URI = ""
+MONGO_DB = ""
+MONGO_COLLECTION = ""
+
+
+# INCIALIZAMAMOS CLIENTE DE MONGO
+client = MongoClient(MONGO_URI)
+db = client[MONGO_DB]
