bb071efd95e73241ee9d9d42cdc3591bd56b858a
AlexVennegas <erick.alexis.venegas@gmail.com>
2023-09-22 09:10:20 +0200

    fix: fixed functions push mongoDB

--- Functions/Push_name.py
+++ Functions/Push_name.py
@@ -3,8 +3,9 @@ from MongoDB.Conexion import *
 # Push data to MongoDB
 def push_name(data, value, clave):
     try:
-        result = db[MONGO_COLLECTION].find_one({"name" : value})
-        if result is None:
+        result = db[MONGO_COLLECTION].find_one({"passport" : data['passport']})
+        result_name_lastname = db[MONGO_COLLECTION].find_one({'fullname': data['name'] + ' ' + data['last_name']})
+        if result is None or result_name_lastname is None:
             db[MONGO_COLLECTION].insert_one(data)            
         else:
             new_data = {**result, **data}
@@ -30,13 +31,13 @@ def push_address(data, value, clave):
 # Push data to MongoDB
 def push_another(data, value, clave):
     try:
-        result = db[MONGO_COLLECTION].find_one({"passport" : value})
+        result = db[MONGO_COLLECTION].find_one({"fullname" : value})
         if result is None:
             db[MONGO_COLLECTION].insert_one(data)            
         else:
             new_data = {**result, **data}
             # Actualiza el documento en la base de datos con los nuevos datos
-            db[MONGO_COLLECTION].update_one({"passport": value}, {"$set": new_data})
+            db[MONGO_COLLECTION].update_one({"fullname": value}, {"$set": new_data})
     except Exception as e:
         print(f"Error al acceder a la base de datos: {str(e)}")
 
@@ -52,7 +53,7 @@ def push_fullname(data, value, clave):
     except Exception as e:
         print(f"Error al acceder a la base de datos: {str(e)}")
 
-
+# Push data to MongoDB
 def push_passport(data, value, clave):
     try:
         result = db[MONGO_COLLECTION].find_one({"passport" : value})
