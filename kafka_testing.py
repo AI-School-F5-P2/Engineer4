5325cf26b0989900996953d3fad8e14f88aebb2b
Jose Quijado <chefito@MacBook-Pro-de-Jose.local>
2023-09-25 08:40:32 +0200

    fix:eliminadas clases no usadas

---
+++ kafka_testing.py
@@ -0,0 +1,11 @@
+from kafka import KafkaConsumer
+import json
+
+consumer = KafkaConsumer("probando", bootstrap_servers=['localhost:29092'], value_deserializer=lambda x: json.loads(x))
+
+for msg in consumer:
+    print(msg.value)
+    print("recibido")
+
+
+    
--- main.py
+++ main.py
@@ -1,46 +1,21 @@
-import json
-from auxiliares.Push_name import push_name, push_address, push_fullname, push_passport, push_another
-from conf.kafka_conf import *
-import sys
-import faust
+#import json
+#from auxiliares.Push_name import push_name, push_address, push_fullname, push_passport, push_another
+from conf.kafka_conf import app, topic
 
 # funcion para guardar los datos en un json
-def save_data(data, path):
-    with open(path, "w", encoding="utf-8") as archivo_json:
-        json.dump(data, archivo_json, ensure_ascii=False)
+#def save_data(data, path):
+#    with open(path, "w", encoding="utf-8") as archivo_json:
+#        json.dump(data, archivo_json, ensure_ascii=False)
 
-# funcion para iterar en la data        
-def processData(data):
-    try:
-    # Procesamos cada data que nos llega y se almacena en bruto
-        # save_data(data, "data.json")
-        for clave, valor in data.items():
-            if 'name' in data and 'last_name' in data and 'sex' in data and 'telfnumber' in data and 'passport' in data and 'email' in data :
-                push_name(data, valor, clave)
-            elif 'passport' in data and 'IBAN' in data and 'salary' in data :
-                push_passport(data, valor, clave)
-            elif 'fullname' in data and 'city' in data and 'address' in data:
-                push_fullname(data, valor, clave)
-            elif 'fullname' in data and 'company' in data and 'company address' in data and 'company_telfnumber' in data and 'company_email' in data and 'job' in data:
-                push_another(data, valor, clave)
-            elif 'address' in data and 'IPv4' in data:
-                push_address(data, valor, clave)
-    except Exception as e:
-        print(e)
-        pass
-        
+        # faust -A main worker
 # Define a Faust table to store consumed data
-# faust -A main worker
+
 @app.agent(topic)
 async def consume_messages(messages):
-    '''Consume messages from a Kafka topic
-    To start the application enter the following command: 
-    faust -A main worker
-    '''
     try:
         async for message in messages:
             data = message
-            processData(data)
+            #processData(data)
             print(f'Received message: {data}')
     except Exception as e:
         print(e)
